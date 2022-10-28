e5b66d412b11730403384d14dde120c5db070264
SrikruthiV <112650894+SrikruthiV@users.noreply.github.com>
2022-10-27 23:04:44 -0700

    countUrinals First Test Commit

--- urinal.java
+++ urinal.java
@@ -16,11 +16,47 @@ public class urinal
 			else
 			{
 				c=1;
+				break;
 			}
 		}
 		if(c==0)
 			return true;
 		else
 			return false;
+	}
+	
+	public static int countUrinals(String s)
+	{
+		String orgStr = s;
+		int countMen = 0;
+		for(int i=0;i<s.length()-1;i++) 
+		{
+			if(i==0)
+			{
+				if(s.charAt(i)=='0' && s.charAt(i+1)=='0')
+				{
+					s = '1'+ s.substring(i+1);
+					countMen+=1;
+				}
+			}
+			else
+			{
+				if(s.charAt(i)=='0' && s.charAt(i-1)=='0' && s.charAt(i+1)=='0')
+				{
+					s = s.substring(0,i-1)+ '1' + s.substring(i+1);
+					countMen+=1;
+				}
+			}
+		}
+		int m = s.length();
+		if(s.charAt(m-1)=='0' && s.charAt(m-2)=='0')
+		{
+			s = s.substring(0,m-1)+'1';
+			countMen+=1;
+		}
+		if(countMen==0)
+			return 0;
+		else
+			return countMen;
 	}
 }
--- urinalsTest.java
+++ urinalsTest.java
@@ -24,7 +24,16 @@ class urinalsTest {
 	void testValidate3()
 	{
 		urinal ob = new urinal();
-		System.out.println("==== SRIKRUTHI VEDANTHAM ==== Validate String Second Test");
+		System.out.println("==== SRIKRUTHI VEDANTHAM ==== Validate String Third Test");
 		assertEquals(true, ob.validateString("100011"));
 	}
+	
+	@Test
+	void testcountUrinals1()
+	{
+		urinal ob = new urinal();
+		System.out.println("==== SRIKRUTHI VEDANTHAM ==== Count Urinals First Test");
+		assertEquals(3, ob.validateString("10001"));
+	}
+	
 }
